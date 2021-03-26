5fd2bcb9ca0b84216a8b405e218f6837944309d4
jazbassi1 <65470680+jazbassi1@users.noreply.github.com>
2021-03-26 18:04:43 +0000

    Add files via upload

---
+++ turtle-square.py
@@ -0,0 +1,11 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
+my_turtle.right(90)
+my_turtle.forward(100)
